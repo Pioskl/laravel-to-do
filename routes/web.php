aa7545db8e1b88995580e235e51af9e13feb9d92
Pioskl <115486249+Pioskl@users.noreply.github.com>
2025-03-19 00:11:29 +0100

    To do list completed

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Http\Controllers\ItemController;
+use App\Models\Item;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    $items = Item::orderBy('created_at', 'desc')->get();
+    return Inertia::render('Welcome', ['items' => $items]);
 })->name('home');
+
+
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+
+Route::prefix('item')->group(function () {
+    Route::post('store', [ItemController::class, 'store'])->name('item.store');
+    Route::put('{id}', [ItemController::class, 'update'])->name('item.update');
+    Route::delete('{id}', [ItemController::class, 'destroy'])->name('item.destroy');
+});
 
